75c06782aca0581244d17ed4be9952db21e91dcc
lixiaoniu <lixiaoniu@edaijia-inc.cn>
2017-11-17 19:14:06 +0800

    枚举和代理

---
+++ src/kotlinOffical/FunDemo.kt
@@ -0,0 +1,18 @@
+package kotlinOffical
+
+fun main(args: Array<String>){
+    printSum(2, 3)
+}
+
+/*fun add(a: Int, b: Int): Int {
+    *//*asd*//*
+    return a + b;
+}*/
+
+fun add2(a: Int, b: Int) = a + b;
+
+fun printSum(a: Int, b: Int): Unit {
+    println("sum of ${a} and ${b} is ${a + b}")
+}
+
+
---
+++ src/kotlinOffical/FunDemo2.kt
@@ -0,0 +1,8 @@
+package kotlinOffical
+
+fun main(args: Array<String>) {
+
+}
+fun add3(a: Int, b: Int): Int {
+    return a + b;
+}
